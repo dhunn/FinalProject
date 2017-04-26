f3fc366eaf20fa659bcc78634e4ab613443d43cc
dhunn <Danny Hunn@192.168.1.9>
2017-04-26 00:16:30 -0500

    V2

--- src/finalProject/Main.java
+++ src/finalProject/Main.java
@@ -3,7 +3,7 @@ package finalProject;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println(Attribute.Green_Eyes);
 
 	}
 
--- src/finalProject/Person.java
+++ src/finalProject/Person.java
@@ -1,32 +1,129 @@
 package finalProject;
 
 public class Person {
-	
+
 	private String fName;
 	private String lName;
+	private int attributes;
+	private int height;
 	private int age;
-	private char sex;
-	private int zipCode;
-	private String race;
-	private char seeking;
-	private int heightInches;
-	private String eyeColor;
-	private int compatability;
-	
-	public Person(){
-		
-	}
-	public Person(String fName, String lName, int age, char sex, int zipCode, String race, char seeking,
-			int heightInches, String eyeColor) {
+
+	Person(String fName, String lName, int attributes, int age, int height) {
 		this.fName = fName;
 		this.lName = lName;
+		this.attributes = attributes;
 		this.age = age;
-		this.sex = sex;
-		this.zipCode = zipCode;
-		this.race = race;
-		this.seeking = seeking;
-		this.heightInches = heightInches;
-		this.eyeColor = eyeColor;
+		this.height = height;
+	}
+
+	public int compareTo(Person them) {
+		int count = 0;
+		int temp = them.getAttributes();
+		while (temp > 0) {
+			temp &= (temp - 1);
+			count++;
+		}
+
+		if (them.getAge() == this.age)
+			count++;
+		if (them.getHeight() == this.height)
+			count++;
+		return count;
+	}
+
+	public void setEyeColor(int num) {
+		switch (num) {
+		case 1:
+			this.attributes += Attribute.Brown_Eyes;
+			break;
+		case 2:
+			this.attributes += Attribute.Blue_Eyes;
+			break;
+		case 3:
+			this.attributes += Attribute.Green_Eyes;
+			break;
+		case 4:
+			this.attributes += Attribute.Hazel_Eyes;
+			break;
+		case 5:
+			this.attributes += Attribute.Grey_Eyes;
+			break;
+
+		default:
+			break;
+		}
+	}
+	
+	public void setHairColor(int num){
+		switch (num) {
+		case 1:
+			this.attributes += Attribute.Brown_Hair;
+			break;
+		case 2:
+			this.attributes += Attribute.Blond_Hair;
+			break;
+		case 3:
+			this.attributes += Attribute.Red_Hair;
+			break;
+		case 4:
+			this.attributes += Attribute.Grey_Hair;
+			break;
+		case 5:
+			this.attributes += Attribute.Black_Hair;
+			break;
+
+		default:
+			break;
+		}
+		
+	}
+	public void setRace(int num){
+		switch (num) {
+		case 1:
+			this.attributes += Attribute.Race_Asain;
+			break;
+		case 2:
+			this.attributes += Attribute.Race_Black;
+			break;
+		case 3:
+			this.attributes += Attribute.Race_NativeAmerican;
+			break;
+		case 4:
+			this.attributes += Attribute.Race_White;
+			break;
+		case 5:
+			this.attributes += Attribute.Race_PacificIslander;
+			break;
+
+		default:
+			break;
+		}
+	}
+	public void setSex(int num){
+		switch (num) {
+		case 1:
+			this.attributes += Attribute.Sex;
+			break;
+		case 2:
+			
+			break;
+
+		default:
+			break;
+		}
+	}
+	public void setCorrectiveLenses(int num){
+		switch (num) {
+		case 1:
+			this.attributes += Attribute.Corrective_Lenses;
+			break;
+		case 2:
+			
+			break;
+
+		default:
+			break;
+		}
 	}
 
 	public String getfName() {
@@ -45,82 +142,28 @@ public class Person {
 		this.lName = lName;
 	}
 
+	public int getAttributes() {
+		return attributes;
+	}
+
+	public void setAttributes(int attributes) {
+		this.attributes = attributes;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
 	public int getAge() {
 		return age;
 	}
 
 	public void setAge(int age) {
 		this.age = age;
-	}
-
-	public char getSex() {
-		return sex;
-	}
-
-	public void setSex(char sex) {
-		this.sex = sex;
-	}
-
-	public int getZipCode() {
-		return zipCode;
-	}
-
-	public void setZipCode(int zipCode) {
-		this.zipCode = zipCode;
-	}
-
-	public char getSeeking() {
-		return seeking;
-	}
-
-	public void setSeeking(char seeking) {
-		this.seeking = seeking;
-	}
-
-	public int getHeightInches() {
-		return heightInches;
-	}
-
-	public void setHeightInches(int heightInches) {
-		this.heightInches = heightInches;
-	}
-
-	public String getRace() {
-		return race;
-	}
-
-	public String getEyeColor() {
-		return eyeColor;
-	}
-	private int setByte(int b, int pos){
-		b |= 1 << pos;
-		return b;
-	}
-	private int clearByte(int b, int pos){
-		b &= ~(1 << pos);
-		return b;
-	}
-	public void setCompatability(int num){
-		this.compatability =num;
-	}
-	public int getCompatability(){
-		return this.compatability;
-	}
-	public int compairTo(Person p){
-		int same = 0;
-		
-		if(this.age == p.age) setByte(same, 0) ;
-		if(this.sex==p.sex) setByte(same, 1);
-		if(this.zipCode == p.zipCode) setByte(same, 2);
-		if(this.race.equals(p.race)) setByte(same, 3);
-		if(this.seeking == p.seeking) setByte(same, 4);
-		if(this.heightInches == p.heightInches) setByte(same, 5);
-		if(this.eyeColor.equals(p.eyeColor)) setByte(same, 6);
-		
-		
-		
-		
-		return same;
 	}
 
 }
