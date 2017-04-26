c443176a1fda196e52f5408f3f610476aabea4db
dhunn <Danny Hunn@192.168.1.9>
2017-04-26 00:52:53 -0500

    To Battle

---
+++ src/finalProject/Attribute.java
@@ -0,0 +1,40 @@
+package finalProject;
+
+public class Attribute {
+	
+	public static final int Brown_Eyes =1;
+	public static final int	Blue_Eyes = Brown_Eyes << 1;
+	public static final int	Green_Eyes = Brown_Eyes << 2;
+	public static final int Hazel_Eyes = Brown_Eyes << 3;
+	public static final int	Grey_Eyes = Brown_Eyes << 4;
+	public static final int	Brown_Hair = Brown_Eyes << 5;
+	public static final int	Blond_Hair = Brown_Eyes << 6;
+	public static final int	Red_Hair = Brown_Eyes << 7;
+	public static final int	Grey_Hair = Brown_Eyes << 8;
+	public static final int	Black_Hair = Brown_Eyes << 9;
+	public static final int Race_White = Brown_Eyes << 10;
+	public static final int Race_Asain = Brown_Eyes << 11;  
+	public static final int Race_NativeAmerican = Brown_Eyes << 12;
+	public static final int	Race_Black = Brown_Eyes << 13;
+	public static final int Race_PacificIslander =Brown_Eyes << 14;
+	public static final int Sex = Brown_Eyes << 15; // if set to its male or 1 if not set it is 0 or female cause of the naughty bits
+	public static final int Corrective_Lenses = Brown_Eyes << 16; // 1 is uses lenses
+	public static final int Ethnic_Arabs = Brown_Eyes << 17;
+	public static final int Ethnic_Hispanic =Brown_Eyes << 18;
+	public static final int	Ethnic_British = Brown_Eyes << 19;
+	public static final int Ethnic_Han_Chinese = Brown_Eyes << 20;
+	public static final int Ethnic_Bengalis = Brown_Eyes << 21;
+	public static final int Ethnic_Russian = Brown_Eyes << 22;
+	public static final int Ethnic_Japanese = Brown_Eyes << 23;
+	public static final int Ethnic_Punjabi = Brown_Eyes << 24;
+	public static final int	Ethnic_Javanese = Brown_Eyes << 25;
+	public static final int Ethnic_English = Brown_Eyes << 26;
+	public static final int Ethnic_French =Brown_Eyes << 27;
+	public static final int Ethnic_German = Brown_Eyes << 28;
+	public static final int Language_English = Brown_Eyes << 29;
+	public static final int Language_Mandarin = Brown_Eyes << 30;
+	public static final int Language_Spanish =  Brown_Eyes << 31;
+	
+	
+	
+}
